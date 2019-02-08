ac25a9d40a4a5063079e5d9c504c736e1e8c9c35
Pheonixx77 <farid.najar@u-psud.fr>
2019-02-08 17:49:33 +0100

    jai souffert

--- TP2/sample_code_submission/model.py
+++ TP2/sample_code_submission/model.py
@@ -10,12 +10,12 @@ import pickle
 import numpy as np   # We recommend to use numpy arrays
 from os.path import isfile
 from sklearn.base import BaseEstimator
-
+from sklearn.base import BaseEstimator
 class model (BaseEstimator):
     def __init__(self):
         '''
         This constructor is supposed to initialize data members.
-        Use triple quotes for function documentation. 
+        Use triple quotes for function documentation.
         '''
         self.num_train_samples=0
         self.num_feat=1
